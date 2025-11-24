05a4135d0efe6f88bd3f29696de0aab31834bf04
ethanrodriguez5433 <ethanrodriguez5433@gmail.com>
2025-11-24 12:31:07 -0600

    Create main layout

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,12 @@
 FROM python:3.12-slim
 
+RUN apt-get update && \
+    apt-get install -y python3-tk tk && \
+    apt-get clean && rm -rf /var/lib/apt/lists/*
+
 WORKDIR /pdfapp
 COPY . /pdfapp
 
 RUN pip install -r requirements.txt
 
-CMD["python", "main.py"]
+CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -31,10 +31,54 @@ except:
 
 root = tk.Tk()
 root.title("PDF Reader")
-root.geometry("450x250")
+root.geometry("800x500")
+root.config(bg="skyblue")
 root.tk.call('tk','scaling', 1.3)
 
-button = tk.Button(root, text="Select PDF", command=select_pdf)
-button.pack(padx=20, pady=20)
+
+# -----------------------------------------
+# LEFT SIDEBAR (fixed width, full height)
+# -----------------------------------------
+sidebar = tk.Frame(root, bg="#d9d9d9", width=550)
+sidebar.pack(side="left", fill="y")
+
+# Make the frame enforce its width even when children expand
+sidebar.pack_propagate(False)
+
+# -----------------------------------------
+# TWO BUTTONS THAT AUTO-FILL VERTICALLY
+# -----------------------------------------
+selecticon = tk.PhotoImage(file="selecticon.png")
+btn1 = tk.Button(
+    sidebar,
+    command=select_pdf,
+    image = selecticon,
+    bg="#C1E5F5",          # green
+    fg="black",            # white text
+    activebackground="#439FD5",
+    activeforeground="white",
+    bd=0                   # optional (flat look)
+)
+btn1.image = selecticon
+btn1.pack(fill="both", expand=True)
+
+bookmarkicon = tk.PhotoImage(file="bookmarkicon.png")
+btn2 = tk.Button(
+    sidebar,
+    image=bookmarkicon,
+    command=select_pdf,
+    bg="#C1E5F5",          # green
+    fg="black",            # white text
+    activebackground="#439FD5",
+    activeforeground="white",
+    bd=0)
+btn2.image=bookmarkicon
+btn2.pack(fill="both", expand=True)
+
+# -----------------------------------------
+# MAIN AREA
+# -----------------------------------------
+main_area = tk.Frame(root, bg="white")
+main_area.pack(side="right", fill="both", expand=True)
 
 root.mainloop()
