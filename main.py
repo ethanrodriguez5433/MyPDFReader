46d7b9f219a3f8f7dacc9b4bd74e4181c5f78ecf
ethanrodriguez5433 <ethanrodriguez5433@gmail.com>
2025-11-25 17:35:13 -0600

    play button added

--- main.py
+++ main.py
@@ -3,16 +3,23 @@ import ctypes
 from tkinter import filedialog
 import pyttsx3
 from pypdf import PdfReader
+import threading
 
 engine = pyttsx3.init()
 
+
 def select_pdf():
+    global readpath
     filepath = filedialog.askopenfilename(
         filetypes=[("PDF Files", "*.pdf")]
     )
     if not filepath:
         return
-    read_pdf(filepath)
+    readpath = filepath
+    #read_pdf(filepath)
+
+def start_reading():
+    read_pdf(readpath)
 
 def read_pdf(path):
     reader = PdfReader(path)
@@ -23,6 +30,30 @@ def read_pdf(path):
     engine.say(text)
     engine.runAndWait()
 
+def pause_pdf(path):
+    engine.stop()
+    return
+
+def toggle_play_pause():
+    global is_playing, readpath
+
+    if is_playing:
+        play_pause_btn.config(image=play_icon)
+        is_playing = False
+        engine.stop()
+        return
+
+    else:
+        if "readpath" not in globals():
+            print("No PDF selected yet.")
+            return
+        play_pause_btn.config(image=pause_icon)
+        is_playing = True
+
+        
+        t = threading.Thread(target=start_reading)
+        t.daemon = True
+        t.start()
 # Fix blurry window on Windows
 try:
     ctypes.windll.shcore.SetProcessDpiAwareness(1)
@@ -48,8 +79,8 @@ sidebar.pack_propagate(False)
 # -----------------------------------------
 # TWO BUTTONS THAT AUTO-FILL VERTICALLY
 # -----------------------------------------
-selecticon = tk.PhotoImage(file="selecticon.png")
-btn1 = tk.Button(
+selecticon = tk.PhotoImage(file="icons/selecticon.png")
+selectbutton = tk.Button(
     sidebar,
     command=select_pdf,
     image = selecticon,
@@ -59,11 +90,11 @@ btn1 = tk.Button(
     activeforeground="white",
     bd=0                   # optional (flat look)
 )
-btn1.image = selecticon
-btn1.pack(fill="both", expand=True)
+selectbutton.image = selecticon
+selectbutton.pack(fill="both", expand=True)
 
-bookmarkicon = tk.PhotoImage(file="bookmarkicon.png")
-btn2 = tk.Button(
+bookmarkicon = tk.PhotoImage(file="icons/bookmarkicon.png")
+boomarkbutton = tk.Button(
     sidebar,
     image=bookmarkicon,
     command=select_pdf,
@@ -72,13 +103,26 @@ btn2 = tk.Button(
     activebackground="#439FD5",
     activeforeground="white",
     bd=0)
-btn2.image=bookmarkicon
-btn2.pack(fill="both", expand=True)
+boomarkbutton.image=bookmarkicon
+boomarkbutton.pack(fill="both", expand=True)
 
 # -----------------------------------------
 # MAIN AREA
 # -----------------------------------------
 main_area = tk.Frame(root, bg="white")
 main_area.pack(side="right", fill="both", expand=True)
+
+play_icon = tk.PhotoImage(file="icons/play.png")
+pause_icon = tk.PhotoImage(file="icons/pause.png")
+
+is_playing = False
+
+play_pause_btn = tk.Button(
+    main_area,
+    image=play_icon,
+    command=toggle_play_pause,
+    bd=0
+)
+play_pause_btn.pack(pady=40)
 
 root.mainloop()
