79bfef2662532b63015c3d7f318e76d7016b8b30
ethanrodriguez5433 <ethanrodriguez5433@gmail.com>
2025-11-30 01:33:02 -0600

    add progress bar  and reading frame

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 import ctypes
 from tkinter import filedialog
 import pyttsx3
@@ -65,6 +66,10 @@ def read_pdf(path):
         current_index += 1
         update_line_window()
 
+        progress_percent = (current_index/total_lines) * 100
+        progress['value'] = progress_percent
+        root.update_idletasks()
+
 def pause_pdf(path):
     global engine
     try:
@@ -111,9 +116,9 @@ root.config(bg="skyblue")
 root.tk.call('tk','scaling', 1.3)
 
 
-# -----------------------------------------
+###########################################
 # LEFT SIDEBAR (fixed width, full height)
-# -----------------------------------------
+###########################################
 sidebar = tk.Frame(root, bg="#d9d9d9", width=550)
 sidebar.pack(side="left", fill="y")
 
@@ -150,12 +155,15 @@ boomarkbutton = tk.Button(
 boomarkbutton.image=bookmarkicon
 boomarkbutton.pack(fill="both", expand=True)
 
-# -----------------------------------------
+############################################
 # MAIN AREA
-# -----------------------------------------
+############################################
 main_area = tk.Frame(root, bg="white")
 main_area.pack(side="right", fill="both", expand=True)
 
+# ------------------------------------------
+# Information Frame
+# ------------------------------------------
 information_frame = tk.Frame(main_area, bg="white")
 information_frame.pack(side="top", fill="x", pady=20)
 
@@ -168,15 +176,44 @@ currently_reading = tk.Label(information_frame,
                              padx=50)
 currently_reading.pack(fill="x")
 
+# -------------------------------------------
+# Reading Frame
+# -------------------------------------------
+reading_frame = tk.Frame(
+    main_area,
+    bg="white",
+    highlightbackground="black",
+    highlightthickness=10
+)
+reading_frame.pack(side="top",
+                   fill="both",
+                   expand=True,
+                   pady=20,
+                   padx=220)
+
+# Inside the middle frame, add 3 labels
+label1 = tk.Label(reading_frame, text="line1", bg="white", font=("Aptos(Body)", 35), fg="#439FD5")
+label2 = tk.Label(reading_frame, text="line2", bg="white", font=("Aptos(Body)", 35))
+label3 = tk.Label(reading_frame, text="line3", bg="white", font=("Aptos(Body)", 35))
+
+# Pack labels vertically with spacing
+label1.pack(fill="both", expand=True)
+label2.pack(fill="both", expand=True)
+label3.pack(fill="both", expand=True)
+# ------------------------------------------
+# Control Panel
+# ------------------------------------------
+controls_frame = tk.Frame(main_area, bg="white")
+controls_frame.pack(side="bottom", fill="x", pady=20)
+
+#pause/play icon
 original_play_icon = tk.PhotoImage(file="icons/play.png")
 original_pause_icon = tk.PhotoImage(file="icons/pause.png")
 
-play_icon = original_play_icon.subsample(2,2)
-pause_icon = original_pause_icon.subsample(2,2)
+play_icon = original_play_icon.subsample(4,4)
+pause_icon = original_pause_icon.subsample(4,4)
 
 is_playing = False
-controls_frame = tk.Frame(main_area, bg="white")
-controls_frame.pack(side="bottom", fill="x", pady=20)
 
 play_pause_btn = tk.Button(
     controls_frame,
@@ -185,5 +222,23 @@ play_pause_btn = tk.Button(
     bd=0
 )
 play_pause_btn.pack(pady=40)
+
+#Progress bar
+style = ttk.Style()
+style.theme_use('default')
+style.configure(
+    "Custom.Horizontal.TProgressbar",
+    throughcolor="#C1E5F5",
+    background="#439FD5",
+    thickness=50
+)
+progress = tk.ttk.Progressbar(controls_frame, 
+                              orient = "horizontal",
+                              length = 1000, 
+                              mode = "determinate", 
+                              maximum=100,
+                              style="Custom.Horizontal.TProgressbar")
+progress.pack(pady=10)
+
 
 root.mainloop()
